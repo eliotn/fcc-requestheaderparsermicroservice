5c4018f4a36942b122b0ed25b0c9443649fbec45
Eliot Glairon <ejg_dnd@yahoo.com>
2016-10-02 15:03:38 +0000

    final tuning changes

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ var express = require('express');
 var uaparser = require('ua-parser');
 var forwarded = require('forwarded-for')
 var app = express();
-
+function n(val) {
+    return (val)?val:null;
+}
 app.get('/*', function (req, res) {
   var uadata = uaparser.parse(req.get('user-agent'));
   var ipaddress = forwarded(req, req.headers).ip;
-  res.send(JSON.stringify({"ipaddress":ipaddress,
-    "language":req.get('accept-language'),
-    "browser":uadata.ua.toString(),
-    "os":uadata.os.toString(),
-    "device":uadata.device.family
-  }));
+  res.set({'Content-Type': 'application/json'});
+  res.send({"ipaddress":n(ipaddress),
+    "language":n(req.get('accept-language')),
+    "browser":n(uadata.ua.toString()),
+    "os":n(uadata.os.toString()),
+    "device":n(uadata.device.family)
+  });
 });
 
 app.listen(process.env.PORT, function () {
