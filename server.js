8e9f98dee9222daae6cbada5db22862215f77557
Eliot Glairon <ejg_dnd@yahoo.com>
2016-10-02 14:05:01 +0000

    listen to right port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
+app.listen(process.env.port, function () {
   console.log('Example app listening on port 8080!');
 });
